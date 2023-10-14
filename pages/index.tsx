65597af518e3dbd854b995556ffc4897b1b8be4c
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-10-14 08:25:38 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,9 @@ import Hero from "../components/Hero"
 import Productize from "../components/Productize"
 import ToLearn from "../components/ToLearn"
 import Mentors from "../components/Mentors"
-<<<<<<< HEAD
-=======
 import Whyus from "../components/Whyus"
 import Faq from "../components/Faq"
 import Pricing from "../components/Pricing"
->>>>>>> b4bf71bf3240c994200e1483e5b2721bf35205d2
-
 
 const App = () => {
   return (
@@ -18,12 +14,9 @@ const App = () => {
       <Productize/>
       <ToLearn/>
       <Mentors/>
-<<<<<<< HEAD
-=======
       <Whyus />
       <Pricing />
       <Faq />
->>>>>>> b4bf71bf3240c994200e1483e5b2721bf35205d2
     </div>
   )
 }
